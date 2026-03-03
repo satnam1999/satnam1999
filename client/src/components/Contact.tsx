6ad84eb732b65aca7407a73ca0208985ea90982f
satnam1999 <satnampahwa85@gmail.com>
2026-03-03 19:56:27 +0530

    Update Contact.tsx to change href link

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
+            <h1>Contact Us</h1>
             <a href="https://github.com/satnam1999">GitHub</a>
         </div>
     );
