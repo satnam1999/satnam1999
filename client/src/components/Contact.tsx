326637e1b1c39d4caed02bf27d137da29811255d
satnam1999 <satnampahwa85@gmail.com>
2026-03-03 19:56:01 +0530

    Updated the GitHub link in Contact.tsx from https://github.com to https://github.com/satnam1999

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            <h1>Contact Us</h1>
-            <p>For more information, reach out to us through our GitHub link.</p>
             <a href="https://github.com/satnam1999">GitHub</a>
         </div>
     );
