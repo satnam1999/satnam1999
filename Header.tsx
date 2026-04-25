b97cd053ad72e93a35dd332442da03503ce6cf52
Satnam Singh Pahwa <satnampahwa85@gmail.com>
2026-04-25 13:31:59 +0530

    Updated Portfolio link in Header.tsx

---
+++ Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <a href="https://github.com/satnam1999" className="text-gray-900 dark:text-gray-100 font-medium">Portfolio</a>
+        {/* Other navigation items */}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
